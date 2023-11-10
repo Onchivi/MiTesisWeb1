0472d3df3a42be886a3e22de2528a2051c634bf8
Onchivi <sebastian061974@hotmail.com>
2023-11-10 16:51:32 -0500

    cambios DB

--- php/read.php
+++ php/read.php
@@ -1,18 +1,20 @@
 <?php
-include 'conexion.php';
+include 'conexion.php'; // Incluye el script de conexión modificado
 
-$sql = "SELECT nombre, apellido, edad, telefono FROM usuarios";
+$sql = "SELECT id, nombre, apellido, edad, telefono FROM usuarios"; // Tu consulta SQL
 $result = $conn->query($sql);
 
 $usuarios = array();
 
 if ($result->num_rows > 0) {
+    // Recorre los resultados y añádelos al array
     while($row = $result->fetch_assoc()) {
         $usuarios[] = $row;
     }
-    
-} 
-header('Content-Type: application/json');
-echo json_encode($usuarios);
-$conn->close();
+    echo json_encode($usuarios); // Envía los resultados en formato JSON
+} else {
+    echo json_encode([]); // Envía un array vacío si no hay resultados
+}
+
+$conn->close(); // Cierra la conexión
 ?>
